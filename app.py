663228f6ca04ba32367dc34a63c54d0a2105053e
angellenmaebserna-art <angellenmaebserna@asscat.edu.ph>
2025-10-22 09:04:11 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from prophet import Prophet
+import cmdstanpy
+import prophet
 import statsmodels
 from sklearn.metrics import confusion_matrix
 from sklearn.ensemble import RandomForestRegressor
